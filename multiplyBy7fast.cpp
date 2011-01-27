949ea1268bf08c317d6c243e9557a26e6824881b
Kevindra Singh <kevindra.singh@gmail.com>
2011-01-27 18:55:46 +0530

    Multiply By 7 Fast

---
+++ multiplyBy7fast.cpp
@@ -0,0 +1,19 @@
+/**
+  * File Name : multiplyBy7fast.cpp
+  *
+  * Purpose :
+  * Creation Date : 27-01-2011
+  *
+  * Last Modified : Thursday 27 January 2011 06:55:31 PM IST
+  *
+  * Created By : Kevindra Singh <kevindra.singh@gmail.com>
+  */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num = 4;
+  cout<<((num<<3)-num)<<endl;
+  return 0;
+}
