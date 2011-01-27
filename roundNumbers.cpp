ca73bf3ef700d1f1a35a4cd50f6a1def293f81f8
Kevindra Singh <kevindra.singh@gmail.com>
2011-01-27 19:02:29 +0530

    Round the numbers

---
+++ roundNumbers.cpp
@@ -0,0 +1,22 @@
+/**
+  * File Name : roundNumbers.cpp
+  *
+  * Purpose :
+  * Creation Date : 27-01-2011
+  *
+  * Last Modified : Thursday 27 January 2011 07:01:30 PM IST
+  *
+  * Created By : Kevindra Singh <kevindra.singh@gmail.com>
+  */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  float num = 3.5;
+
+  int roundedNum = num+0.5;
+
+  cout<<"Rounded Number of "<<num<<" : "<<roundedNum<<endl;
+  return 0;
+}
