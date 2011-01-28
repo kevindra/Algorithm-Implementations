2cbff9e0ef9e53f6d56f4a6d1fb6927e212a9b01
Kevindra Singh <kevindra.singh@gmail.com>
2011-01-28 11:47:17 +0530

    Adding two numbers without using plus operator

---
+++ addTwoNumbersWithoutPlusOperator.cpp
@@ -0,0 +1,22 @@
+/**
+  * File Name : addTwoNumbersWithoutPlusOperator.cpp
+  *
+  * Purpose :
+  * Creation Date : 28-01-2011
+  *
+  * Last Modified : Friday 28 January 2011 11:46:49 AM IST
+  *
+  * Created By : Kevindra Singh <kevindra.singh@gmail.com>
+  */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a,b;
+  a=5,b=3;
+  cout<<"Sum : "<<(a-(-b))<<endl;
+  /*cout<<"Sum: "<<(a^b)<<endl;
+  cout<<"Carry: "<<(a&b)<<endl;*/
+  return 0;
+}
