091e8b68f32e4ad55428538da3844796ab6b3b98
Kevindra Singh <kevindra.singh@gmail.com>
2011-01-31 18:59:34 +0530

    Difference b/w an array of pointers and pointer of an array

---
+++ memory_related/pointersToArray.cpp
@@ -0,0 +1,18 @@
+/**
+  * File Name : pointersToArray.cpp
+  *
+  * Purpose :
+  * Creation Date : 31-01-2011
+  *
+  * Last Modified : Monday 31 January 2011 06:58:42 PM IST
+  *
+  * Created By : Kevindra Singh <kevindra.singh@gmail.com>
+  */
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+  int *p[10]; // an array of 10 pointers
+  int (*q)[10]; // a pointer to a 10 element array .. just like q = new int[10];
+}
